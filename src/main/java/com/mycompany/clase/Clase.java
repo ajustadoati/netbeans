44a086d68b62d0f5c44ec0941d6eda997e71991c
Richard <richardroj@gmail.com>
2023-02-18 17:55:30 +0100

    Initial

---
+++ src/main/java/com/mycompany/clase/Clase.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase;
+
+/**
+ * Clase para main
+ * @author richardroj
+ */
+public class Clase {
+
+    /**
+     * 
+     *Method to create main
+    **/
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
